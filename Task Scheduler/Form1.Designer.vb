1e44bb67a40c7a2d98b4eb86209edc27c326dfd2
joel samoita <57324853+joe-samoita@users.noreply.github.com>
2024-03-28 12:31:27 +0300

    Add files via upload

---
+++ Task Scheduler/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtTaskName = New TextBox()
+        txtPriorityLevel = New TextBox()
+        lblTaskName = New Label()
+        LblPriorityLevel = New Label()
+        btnExecute = New Button()
+        Label1 = New Label()
+        btnAddTask = New Button()
+        btnTaskDelete = New Button()
+        txtDelete = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' txtTaskName
+        ' 
+        txtTaskName.Location = New Point(297, 89)
+        txtTaskName.Name = "txtTaskName"
+        txtTaskName.Size = New Size(120, 23)
+        txtTaskName.TabIndex = 0
+        ' 
+        ' txtPriorityLevel
+        ' 
+        txtPriorityLevel.Location = New Point(297, 129)
+        txtPriorityLevel.Name = "txtPriorityLevel"
+        txtPriorityLevel.Size = New Size(120, 23)
+        txtPriorityLevel.TabIndex = 1
+        ' 
+        ' lblTaskName
+        ' 
+        lblTaskName.AutoSize = True
+        lblTaskName.Location = New Point(198, 92)
+        lblTaskName.Name = "lblTaskName"
+        lblTaskName.Size = New Size(67, 15)
+        lblTaskName.TabIndex = 3
+        lblTaskName.Text = "Task Name:"
+        ' 
+        ' LblPriorityLevel
+        ' 
+        LblPriorityLevel.AutoSize = True
+        LblPriorityLevel.Location = New Point(198, 132)
+        LblPriorityLevel.Name = "LblPriorityLevel"
+        LblPriorityLevel.Size = New Size(78, 15)
+        LblPriorityLevel.TabIndex = 4
+        LblPriorityLevel.Text = "Priority Level:"
+        ' 
+        ' btnExecute
+        ' 
+        btnExecute.Location = New Point(270, 295)
+        btnExecute.Name = "btnExecute"
+        btnExecute.Size = New Size(105, 33)
+        btnExecute.TabIndex = 6
+        btnExecute.Text = "Execute"
+        btnExecute.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Trebuchet MS", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(226, 43)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(179, 29)
+        Label1.TabIndex = 7
+        Label1.Text = "Task Scheduler"
+        ' 
+        ' btnAddTask
+        ' 
+        btnAddTask.Location = New Point(270, 172)
+        btnAddTask.Name = "btnAddTask"
+        btnAddTask.Size = New Size(104, 30)
+        btnAddTask.TabIndex = 8
+        btnAddTask.Text = "Add Tasks"
+        btnAddTask.UseVisualStyleBackColor = True
+        ' 
+        ' btnTaskDelete
+        ' 
+        btnTaskDelete.BackColor = SystemColors.Control
+        btnTaskDelete.Location = New Point(274, 266)
+        btnTaskDelete.Name = "btnTaskDelete"
+        btnTaskDelete.Size = New Size(100, 23)
+        btnTaskDelete.TabIndex = 9
+        btnTaskDelete.Text = "Delete Task"
+        btnTaskDelete.UseVisualStyleBackColor = False
+        ' 
+        ' txtDelete
+        ' 
+        txtDelete.Location = New Point(296, 219)
+        txtDelete.Name = "txtDelete"
+        txtDelete.Size = New Size(121, 23)
+        txtDelete.TabIndex = 10
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(176, 222)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(100, 15)
+        Label2.TabIndex = 11
+        Label2.Text = "Enter Task Name :"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(652, 356)
+        Controls.Add(Label2)
+        Controls.Add(txtDelete)
+        Controls.Add(btnTaskDelete)
+        Controls.Add(btnAddTask)
+        Controls.Add(Label1)
+        Controls.Add(btnExecute)
+        Controls.Add(LblPriorityLevel)
+        Controls.Add(lblTaskName)
+        Controls.Add(txtPriorityLevel)
+        Controls.Add(txtTaskName)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtTaskName As TextBox
+    Friend WithEvents txtPriorityLevel As TextBox
+    Friend WithEvents lblTaskName As Label
+    Friend WithEvents LblPriorityLevel As Label
+    Friend WithEvents btnExecute As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnAddTask As Button
+    Friend WithEvents btnTaskDelete As Button
+    Friend WithEvents txtDelete As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Task Scheduler/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    ' Declare a Queue to store task names and priorities
+    Private taskQueue As New Queue(Of Tuple(Of String, Integer))()
+
+    Private Sub btnAddTask_Click(sender As Object, e As EventArgs) Handles btnAddTask.Click
+        ' Your existing code for adding a task to the queue goes here
+
+        Dim stTaskName As String
+        Dim intPriorityLevel As Integer
+
+        ' Retrieve task information from text boxes
+        stTaskName = txtTaskName.Text
+
+        ' Convert text input to Integer for Priority Level
+        If Integer.TryParse(txtPriorityLevel.Text, intPriorityLevel) Then
+            ' Successfully converted text input to Integer
+            ' Add task name and priority to the queue
+            taskQueue.Enqueue(New Tuple(Of String, Integer)(stTaskName, intPriorityLevel))
+
+            ' Clear text fields
+            txtTaskName.Clear()
+            txtPriorityLevel.Clear()
+
+            ' Display a message to indicate successful addition to the queue
+            MessageBox.Show($"Task '{stTaskName}' with Priority Level {intPriorityLevel} added to the queue.")
+        Else
+            ' Conversion failed, handle the error (e.g., display an error message)
+            MessageBox.Show("Please enter a valid numeric value for Priority Level.")
+        End If
+
+        ' This event handler remains unchanged for adding tasks
+    End Sub
+
+    Private Sub btnTaskDelete_Click(sender As Object, e As EventArgs) Handles btnTaskDelete.Click
+        ' Retrieve task name to be deleted from text box
+        Dim taskToDelete As String = txtDelete.Text
+
+        ' Check if the queue contains any tasks
+        If taskQueue.Count > 0 Then
+            ' Flag to track if task is found and deleted
+            Dim taskFound As Boolean = False
+
+            ' Iterate through the queue to find and remove the task
+            Dim queueLength As Integer = taskQueue.Count
+            For i As Integer = 0 To queueLength - 1
+                Dim task As Tuple(Of String, Integer) = taskQueue.Dequeue()
+                If task.Item1 = taskToDelete Then
+                    ' Task found and removed
+                    taskFound = True
+                    MessageBox.Show($"Task '{taskToDelete}' removed from the queue.")
+                Else
+                    ' Task not found, add it back to the queue
+                    taskQueue.Enqueue(task)
+                End If
+            Next
+
+            ' If task was not found, display message
+            If Not taskFound Then
+                MessageBox.Show($"Task '{taskToDelete}' not found in the queue.")
+            End If
+        Else
+            ' Queue is empty, display message
+            MessageBox.Show("No tasks in the queue to delete.")
+        End If
+
+        ' Clear the text box for next input
+        txtDelete.Clear()
+    End Sub
+
+    Private Sub btnExecute_Click(sender As Object, e As EventArgs) Handles btnExecute.Click
+        ' Your existing code for executing tasks based on priority goes here
+        If taskQueue.Count > 0 Then
+            ' Sort tasks based on priority level before displaying them
+            Dim sortedTasks = taskQueue.OrderBy(Function(task) task.Item2)
+
+            ' Display sorted tasks in the order of priority
+            Dim taskInfo As String = "Tasks to be executed based on priority:" & vbCrLf
+            For Each task In sortedTasks
+                taskInfo &= $"Task Name: {task.Item1}, Priority Level: {task.Item2}" & vbCrLf
+            Next
+
+            MessageBox.Show(taskInfo, "Execution Order")
+        Else
+            MessageBox.Show("No tasks in the queue.", "Queue Empty")
+        End If
+        ' This event handler remains unchanged for executing tasks
+    End Sub
+End Class
